37dc6905db218d1f91c25f328344cffa7a6b46f2
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-04-11 01:46:49 +0900

    first commit of pages

---
+++ mobile.js
@@ -0,0 +1,9 @@
+(function() {
+  $(document).bind('mobileinit', on_mobileinit);
+
+  function on_mobileinit() {
+    $.mobile.defaultPageTransition = 'none';
+  }
+
+})();
+// vim:set ts=8 sts=2 sw=2 tw=0 et:
